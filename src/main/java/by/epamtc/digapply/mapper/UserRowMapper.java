76b49bfb12d53c7a834831cb181a2cf2045ac0ff
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-06-04 20:31:47 +0300

    Implement User entity row mapper

---
+++ src/main/java/by/epamtc/digapply/mapper/UserRowMapper.java
@@ -0,0 +1,20 @@
+package by.epamtc.digapply.mapper;
+
+import by.epamtc.digapply.entity.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper implements RowMapper<User> {
+    @Override
+    public User map(ResultSet resultSet) throws SQLException {
+        User user = new User();
+        user.setUserId(resultSet.getInt("user_id"));
+        user.setEmail(resultSet.getString("email"));
+        user.setPassword(resultSet.getString("password"));
+        user.setName(resultSet.getString("name"));
+        user.setSurname(resultSet.getString("surname"));
+        user.setRoleId(resultSet.getInt("role_id"));
+        return user;
+    }
+}
