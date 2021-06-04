72d5bf922bab71974f06c7d2e2899c5c172b9b54
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-06-04 22:08:08 +0300

    Refactor Service Factory

--- src/main/java/by/epamtc/digapply/service/factory/ServiceFactory.java
+++ src/main/java/by/epamtc/digapply/service/factory/ServiceFactory.java
@@ -4,7 +4,7 @@ import by.epamtc.digapply.service.UserService;
 import by.epamtc.digapply.service.impl.UserServiceImpl;
 
 public class ServiceFactory {
-    private final UserService userService= new UserServiceImpl();
+    private final UserService userService = new UserServiceImpl();
 
     private static class Holder{
         static final ServiceFactory INSTANCE = new ServiceFactory();
