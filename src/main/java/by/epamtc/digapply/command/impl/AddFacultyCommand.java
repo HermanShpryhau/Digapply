5d56ed57e933a89df5642c3a8d9c967a151d22d5
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-07-25 10:50:09 +0300

    Add error data to routing

--- src/main/java/by/epamtc/digapply/command/impl/AddFacultyCommand.java
+++ src/main/java/by/epamtc/digapply/command/impl/AddFacultyCommand.java
@@ -17,47 +17,23 @@ import java.util.stream.Collectors;
 
 public class AddFacultyCommand implements Command {
     private static final Logger logger = LogManager.getLogger();
-    private static final long INVALID_ID = -1L;
-    private static final long DEFAULT_ID = 0;
+    private static final long NEW_FACULTY_ID = 0;
     private static final int INVALID_PLACES_COUNT = -1;
 
     @Override
     public Routing execute(HttpServletRequest request, HttpServletResponse response) {
-        Optional<String> facultyIdString = Optional.ofNullable(request.getParameter(RequestParameter.ID));
-        long facultyId = INVALID_ID;
-        if (facultyIdString.isPresent()) {
-            try {
-                facultyId = facultyIdString.get().equals("") ? DEFAULT_ID :Long.parseLong(facultyIdString.get());
-            } catch (NumberFormatException e) {
-                // TODO Add error info
-                return new Routing(PagePath.ERROR_PAGE, RoutingType.FORWARD);
-            }
-        }
         Optional<String> facultyName = Optional.ofNullable(request.getParameter(RequestParameter.FACULTY_NAME));
         Optional<String> placesString = Optional.ofNullable(request.getParameter(RequestParameter.PLACES_COUNT));
-        int places = INVALID_PLACES_COUNT;
-        if (placesString.isPresent()) {
-            try {
-                places = Integer.parseInt(placesString.get());
-            } catch (NumberFormatException e) {
-                // TODO Add error info
-                return new Routing(PagePath.ERROR_PAGE, RoutingType.FORWARD);
-            }
+        int places = fetchPlacesCount(placesString);
+        if (places == INVALID_PLACES_COUNT) {
+            request.setAttribute(RequestAttribute.ERROR_KEY, ErrorKey.INVALID_PLACES_COUNT);
+            return new Routing(PagePath.ERROR_PAGE, RoutingType.FORWARD);
         }
         Optional<String> shortDescription = Optional.ofNullable(request.getParameter(RequestParameter.SHORT_FACULTY_DESCRIPTION));
         Optional<String> facultyDescription = Optional.ofNullable(request.getParameter(RequestParameter.FACULTY_DESCRIPTION));
 
-        List<Long> subjectIds = Arrays.stream(request.getParameterValues(RequestParameter.SUBJECTS))
-                .mapToLong(Long::parseLong)
-                .boxed()
-                .collect(Collectors.toList());
-        Faculty newFaculty = new Faculty();
-        newFaculty.setFacultyId(facultyId);
-        newFaculty.setFacultyName(facultyName.orElse(null));
-        newFaculty.setFacultyShortDescription(shortDescription.orElse(null));
-        newFaculty.setFacultyDescription(facultyDescription.orElse(null));
-        newFaculty.setPlaces(places);
-        newFaculty.setApplicationClosed(false);
+        List<Long> subjectIds = buildSubjectIdsList(request);
+        Faculty newFaculty = buildFaculty(facultyName, places, shortDescription, facultyDescription);
 
         FacultyService facultyService = ServiceFactory.getInstance().getFacultyService();
         try {
@@ -65,12 +41,42 @@ public class AddFacultyCommand implements Command {
             if (faculty != null) {
                 return new Routing(PagePath.FACULTIES_PAGE_REDIRECT, RoutingType.REDIRECT);
             } else {
-                // TODO set error data
+                request.setAttribute(RequestAttribute.ERROR_KEY, ErrorKey.INVALID_FACULTY_DATA);
                 return new Routing(PagePath.ERROR_PAGE, RoutingType.FORWARD);
             }
         } catch (ServiceException e) {
             logger.error("Unable to save faculty.", e);
             return new Routing(PagePath.ERROR_500_PAGE, RoutingType.FORWARD);
         }
+    }
+
+    private int fetchPlacesCount(Optional<String> placesString) {
+        int places = INVALID_PLACES_COUNT;
+        if (placesString.isPresent()) {
+            try {
+                places = Integer.parseInt(placesString.get());
+            } catch (NumberFormatException e) {
+               return INVALID_PLACES_COUNT;
+            }
+        }
+        return places;
+    }
+
+    private List<Long> buildSubjectIdsList(HttpServletRequest request) {
+        return Arrays.stream(request.getParameterValues(RequestParameter.SUBJECTS))
+                .mapToLong(Long::parseLong)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    private Faculty buildFaculty(Optional<String> facultyName, int places, Optional<String> shortDescription, Optional<String> facultyDescription) {
+        Faculty newFaculty = new Faculty();
+        newFaculty.setFacultyId(NEW_FACULTY_ID);
+        newFaculty.setFacultyName(facultyName.orElse(null));
+        newFaculty.setFacultyShortDescription(shortDescription.orElse(null));
+        newFaculty.setFacultyDescription(facultyDescription.orElse(null));
+        newFaculty.setPlaces(places);
+        newFaculty.setApplicationClosed(false);
+        return newFaculty;
     }
 }
