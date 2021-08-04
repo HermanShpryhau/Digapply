1bbcd46be5077e3719f396da0b597a4b39504d15
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-08-04 17:43:25 +0300

    Add encoding filter

---
+++ src/main/java/by/epamtc/digapply/controller/filter/EncodingFilter.java
@@ -0,0 +1,24 @@
+package by.epamtc.digapply.controller.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+    private String encoding;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        encoding = filterConfig.getInitParameter("request-encoding");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding(encoding);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
