c2bbe690204c64f9bbfe272dbae577f4b20da069
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-08-08 19:33:19 +0300

    Add role row mapper to row mapper factory

--- src/main/java/by/epamtc/digapply/dao/mapper/RowMapperFactory.java
+++ src/main/java/by/epamtc/digapply/dao/mapper/RowMapperFactory.java
@@ -1,6 +1,7 @@
 package by.epamtc.digapply.dao.mapper;
 
 import by.epamtc.digapply.dao.DaoFactory;
+import by.epamtc.digapply.dao.RoleDao;
 import by.epamtc.digapply.entity.*;
 
 public class RowMapperFactory {
@@ -9,6 +10,7 @@ public class RowMapperFactory {
     private final RowMapper<Subject> subjectRowMapper = new SubjectRowMapper();
     private final RowMapper<Application> applicationRowMapper = new ApplicationRowMapper();
     private final RowMapper<Result> resultRowMapper = new ResultRowMapper();
+    private final RowMapper<Role> roleRowMapper = new RoleRowMapper();
 
     /**
      * Gets instance of {@link DaoFactory}.
@@ -56,6 +58,14 @@ public class RowMapperFactory {
      */
     public RowMapper<Result> getResultRowMapper() {
         return resultRowMapper;
+    }
+
+    /**
+     * Gets implementation of {@link RowMapper}
+     * @return {@link RowMapper} implementation for {@link Role} entities.
+     */
+    public RowMapper<Role> getRoleRowMapper() {
+        return roleRowMapper;
     }
 
     private static class Holder {
