252ce9e3e7fa0283709e68a0c64caf92d76f9b14
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-08-12 22:07:30 +0300

    Refactor ID parsing

--- src/main/java/by/epamtc/digapply/command/impl/NewApplicationCommand.java
+++ src/main/java/by/epamtc/digapply/command/impl/NewApplicationCommand.java
@@ -7,6 +7,8 @@ import by.epamtc.digapply.service.ApplicationService;
 import by.epamtc.digapply.service.FacultyService;
 import by.epamtc.digapply.service.ServiceException;
 import by.epamtc.digapply.service.ServiceFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,6 +17,8 @@ import java.util.List;
 import java.util.Optional;
 
 public class NewApplicationCommand implements Command {
+    private static final Logger logger = LogManager.getLogger();
+
     @Override
     public Routing execute(HttpServletRequest request, HttpServletResponse response) {
         ApplicationService applicationService = ServiceFactory.getInstance().getApplicationService();
@@ -24,24 +28,24 @@ public class NewApplicationCommand implements Command {
                 return new Routing(PagePath.ONLY_ONE_APPLICATION_PAGE, RoutingType.FORWARD);
             }
         } catch (ServiceException e) {
+            logger.error("Unable tp check if user already has an application. {}", e.getMessage());
             return Routing.ERROR_500;
         }
         Optional<String> facultyIdString = Optional.ofNullable(request.getParameter(RequestParameter.FACULTY_ID));
-        if (facultyIdString.isPresent()) {
-            try {
-                long facultyId = Long.parseLong(facultyIdString.get());
-                FacultyService facultyService = ServiceFactory.getInstance().getFacultyService();
-                Faculty faculty = facultyService.retrieveFacultyById(facultyId);
-                List<Subject> subjects = facultyService.retrieveSubjectsForFaculty(facultyId);
-                request.setAttribute(RequestAttribute.FACULTY, faculty);
-                request.setAttribute(RequestAttribute.SUBJECTS, subjects);
-            } catch (NumberFormatException e) {
-                return Routing.ERROR_404;
-            } catch (ServiceException e) {
-                return Routing.ERROR_500;
-            }
-        } else {
+        long facultyId =RequestParameterParser.parsePositiveLong(facultyIdString);
+        if (facultyId == RequestParameterParser.INVALID_POSITIVE_LONG) {
             return Routing.ERROR_404;
+        }
+
+        FacultyService facultyService = ServiceFactory.getInstance().getFacultyService();
+        try {
+            Faculty faculty = facultyService.retrieveFacultyById(facultyId);
+            List<Subject> subjects = facultyService.retrieveSubjectsForFaculty(facultyId);
+            request.setAttribute(RequestAttribute.FACULTY, faculty);
+            request.setAttribute(RequestAttribute.SUBJECTS, subjects);
+        } catch (ServiceException e) {
+            logger.error("Unable to retrieve faculty {} data. {}", facultyId, e.getMessage());
+            return Routing.ERROR_500;
         }
         return new Routing(PagePath.APPLICATION_FORM_PAGE, RoutingType.FORWARD);
     }
