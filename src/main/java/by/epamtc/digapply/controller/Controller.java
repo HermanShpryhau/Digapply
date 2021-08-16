dc7770074641e6c5cdbd4403eb9158c0ec3d2e9e
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-08-16 12:34:25 +0300

    Remove unused logger

--- src/main/java/by/epamtc/digapply/controller/Controller.java
+++ src/main/java/by/epamtc/digapply/controller/Controller.java
@@ -15,8 +15,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class Controller extends HttpServlet {
-    private static final Logger logger = LogManager.getLogger(Controller.class);
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         processCommand(req, resp);
