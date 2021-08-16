7ab0b509ce4e9e1c1c89c8f9d8b233181617ec62
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-08-16 19:41:45 +0300

    Make ProxyConnection package private

--- src/main/java/by/epamtc/digapply/dao/connection/ProxyConnection.java
+++ src/main/java/by/epamtc/digapply/dao/connection/ProxyConnection.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Executor;
 
-public class ProxyConnection implements Connection {
+class ProxyConnection implements Connection {
     private static final Logger logger = LogManager.getLogger();
 
     private Connection connection;
