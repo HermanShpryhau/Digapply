0cb88065a767dfa9fb469dac521bd62e215442bc
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-08-19 12:13:00 +0300

    Internationalize date formats

--- src/main/java/by/epamtc/digapply/controller/filter/LocaleFilter.java
+++ src/main/java/by/epamtc/digapply/controller/filter/LocaleFilter.java
@@ -6,18 +6,24 @@ import by.epamtc.digapply.controller.command.SessionAttribute;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
 
 public class LocaleFilter implements Filter {
+    private static final String DEFAULT_LOCALE_PARAM = "defaultLocale";
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        Optional<String> sessionLocale = Optional.ofNullable(request.getParameter(RequestParameter.LOCALE));
-        if (sessionLocale.isPresent()) {
-            request.getSession().setAttribute(SessionAttribute.LOCALE, sessionLocale.get());
+        HttpSession session = request.getSession();
+        if (request.getSession().getAttribute(SessionAttribute.LOCALE) == null || request.getSession().getAttribute(SessionAttribute.LOCALE).equals("")) {
+            session.setAttribute(SessionAttribute.LOCALE, request.getServletContext().getInitParameter(DEFAULT_LOCALE_PARAM));
+        }
+        Optional<String> requestedLocale = Optional.ofNullable(request.getParameter(RequestParameter.LOCALE));
+        if (requestedLocale.isPresent()) {
+            session.setAttribute(SessionAttribute.LOCALE, requestedLocale.get());
             String requestString = removeLocaleParameter(request);
             response.sendRedirect(requestString);
             return;
