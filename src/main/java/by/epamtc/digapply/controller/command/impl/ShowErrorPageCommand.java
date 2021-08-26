09903d7fd1803acb2e565eb72853b39b022c4c2e
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-08-26 09:34:09 +0300

    Fix subject deletion bug

--- src/main/java/by/epamtc/digapply/controller/command/impl/ShowErrorPageCommand.java
+++ src/main/java/by/epamtc/digapply/controller/command/impl/ShowErrorPageCommand.java
@@ -9,10 +9,6 @@ import javax.servlet.http.HttpSession;
 public class ShowErrorPageCommand implements Command {
     @Override
     public Routing execute(HttpServletRequest request, HttpServletResponse response) {
-        HttpSession session = request.getSession();
-        String errorKey = (String) session.getAttribute(SessionAttribute.ERROR_KEY);
-        session.removeAttribute(SessionAttribute.ERROR_KEY);
-        request.setAttribute(RequestAttribute.ERROR_KEY, errorKey);
         return new Routing(PagePath.ERROR_PAGE, RoutingType.FORWARD);
     }
 }
